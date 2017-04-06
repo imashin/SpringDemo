786d06d09d6296b201cbcd9bdbec44093d93ed29
imamura <imamura@192.168.10.4>
2017-04-06 23:44:49 +0900

    Ajax通信を追加

--- demo/src/main/java/com/example/demo/web/controller/Sample01Controller.java
+++ demo/src/main/java/com/example/demo/web/controller/Sample01Controller.java
@@ -1,12 +1,16 @@
 package com.example.demo.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.web.model.Sample01Data;
+import com.example.demo.web.model.Sample01DataList;
 import com.example.demo.web.model.Sample01Form;
 
 @Controller
@@ -15,15 +19,27 @@ public class Sample01Controller {
 	private String CLASS = "Sample01Controller";
 	
 	@RequestMapping(value="/Sample01", method=RequestMethod.GET)
-	public String doGet(@ModelAttribute Sample01Form form, Model model) {
+	public String doGet(Model model) {
 		String METHOD = "doGet";
 		System.out.println(CLASS + " : " + METHOD + ": ### START　###");
+		
+		String[] namelist = new String[]{"imamura", "tanaka", "suzuki"};
+		Sample01DataList sample01DataList = new Sample01DataList();
+		List<Sample01Data> dataList = new ArrayList<Sample01Data>();
+		for (int i = 0; i < namelist.length; i++) {
+			Sample01Data data = new Sample01Data();
+			data.setId(String.valueOf(i));
+			data.setName(namelist[i]);
+			dataList.add(data);
+		}
+		sample01DataList.setDataList(dataList);
+		model.addAttribute("sample01DataList", sample01DataList);
+		
 		System.out.println(CLASS + " : " + METHOD + ": ###　END　###");
 		return "Sample01View";
 	}
 	
 	@RequestMapping(value="/Sample01", method=RequestMethod.POST)
-	@ResponseBody
 	public String doPost(@ModelAttribute Sample01Form form, Model model) {
 		String METHOD = "doPost";
 		System.out.println(CLASS + " : " + METHOD + ": ### START　###");
--- demo/src/main/java/com/example/demo/web/model/Sample02Form.java
+++ demo/src/main/java/com/example/demo/web/model/Sample02Form.java
@@ -2,4 +2,23 @@ package com.example.demo.web.model;
 
 public class Sample02Form {
 
+	private String id = null;
+	private String name = null;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Sample02Form [id=" + id + ", name=" + name + "]";
+	}
+
 }
